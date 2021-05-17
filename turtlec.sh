1a0c321914f1b955fd104d590b8b09eca926be30
diasarw <67229774+diasarw@users.noreply.github.com>
2021-05-17 12:25:52 +0700

    Create turtlec.sh

---
+++ turtlec.sh
@@ -0,0 +1,6 @@
+# turtlecoin
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-static-x64.tar.gz
+tar xf xmrig-6.12.1-linux-static-x64.tar.gz
+cd xmrig-6.12.1
+./xmrig --donate-level 1 -o sg.turtlecoin.herominers.com:10381 -u TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps+c8f5d0028895afadab13552a18b8054594a12bd90e3aec8e2c0ce8729e87802c -p colob -a argon2/chukwav2 -k
