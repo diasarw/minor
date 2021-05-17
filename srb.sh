3b6fb3e201df2ccdba63329e56823fa9cbf403b9
diasarw <67229774+diasarw@users.noreply.github.com>
2021-05-17 13:05:03 +0700

    Create srb.sh

---
+++ srb.sh
@@ -0,0 +1,5 @@
+sudo apt update
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.3/SRBMiner-Multi-0-7-3-Linux.tar.xz
+tar xf SRBMiner-Multi-0-7-3-Linux.tar.xz
+cd SRBMiner-Multi-0-7-3
+./SRBMiner-MULTI --algorithm argon2id_chukwa2 --pool turtlecoin.herominers.com:10380 --wallet TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps+c8f5d0028895afadab13552a18b8054594a12bd90e3aec8e2c0ce8729e87802c 
